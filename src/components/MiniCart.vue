08486a924c5cd84dab21c6c4a7afd27c4e1f6390
Tchouala Nodem Borel <emperialborel@gmail.com>
2023-07-12 06:59:28 -0700

    full design with all the components and pages

---
+++ src/components/MiniCart.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="dropdown-menu p-2" style="min-width:320px; right:0; left: auto;" aria-labelledby="MiniCart">
+        <div>
+            <div class="px-2 d-flex justify-content-between">
+                <div>
+                <strong>Products Title</strong>
+                <br>
+                1 x $23
+                </div>
+                <div>
+                <button href="#" class="btn btn-dark btn-sm">Remove</button>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="d-flex justify-content-between">
+            <span>Total: $23</span>
+            <a href="">Clear Cart</a>
+        </div>
+    </ul>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,13 @@ const router = createRouter({
       component: Index
     },
     {
-      path: '/product-details',
+      path: '/product/:id',
       name: 'product-details',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProductDetails.vue')
+      component: () => import('../views/ProductDetails.vue'),
+      props:true
     }
   ]
 })
